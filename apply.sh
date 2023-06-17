19a564b471deaa9f08505556d087ea14947a0b08
Piotr Ostrowski <piotrostr@google.com>
2023-06-17 16:29:59 +0200

    feat: include a sample application and a CRD to set up pod monitoring for the application

--- apply.sh
+++ apply.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+kubectl get ns workloads || kubectl create ns workloads
 kubectl get ns prometheus || kubectl create ns prometheus
 kubectl get ns grafana || kubectl create ns grafana
 
 kubectl apply -n prometheus -f prometheus-frontend.yaml
 kubectl apply -n grafana -f grafana.yaml
+kubectl apply -n workloads \
+  -f sample-application.yaml \
+  -f pod-monitoring-resource-for-the-sample-application.yaml
